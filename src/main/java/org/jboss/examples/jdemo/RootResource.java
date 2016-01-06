cbe9aaa50be5eebb0be5af3f980dbf5cee9985f6
Christian Posta <christian.posta@gmail.com>
2016-01-06 08:18:58 -0700

    added simple REST service

---
+++ src/main/java/org/jboss/examples/jdemo/RootResource.java
@@ -0,0 +1,37 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package org.jboss.examples.jdemo;
+
+import javax.inject.Singleton;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Singleton
+@Path("/")
+@Produces({"application/json", "text/xml"})
+@Consumes({"application/json", "text/xml"})
+public class RootResource {
+
+
+    @GET
+    @Path("_ping")
+    public String ping() {
+        return "true";
+    }
+
+}
